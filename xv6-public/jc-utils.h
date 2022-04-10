4d4d95a2e0a684e00dff4652b214bb7352380881
Juchan Roh <juchan1220@naver.com>
2022-04-10 16:32:41 +0000

    Fix compile error when LOG_LEVEL is not defined

--- xv6-public/jc-utils.h
+++ xv6-public/jc-utils.h
@@ -1,5 +1,10 @@
+#define LOG_LEVEL_SILENT 0
 #define LOG_LEVEL_DEBUG 1
 #define LOG_LEVEL_VERBOSE 2
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_SILENT
+#endif
 
 #if LOG_LEVEL >= LOG_LEVEL_DEBUG
 #define log_d(args...) cprintf(args) 
